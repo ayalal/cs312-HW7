a99ed8995311da690a840c1ab326139a7200047b
Lorenzo Ayala <Lorenzo.Ayala@a-dec.com>
2020-05-20 11:18:53 -0700

    prepared docs for HW 7

---
+++ webserver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo yum install -y ansible
+
+ssh-keygen -t rsa
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.20
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.21
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.22
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.23
+
+ansible-playbook webserver.yaml -i ./hosts.ini
+
+curl 192.168.1.20
+curl 192.168.1.21
+curl 192.168.1.22
+curl 192.168.1.23
